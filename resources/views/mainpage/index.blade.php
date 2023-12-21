7f48261d5c6a34910bfdcc39007535b68f84e926
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-12-22 02:30:21 +0800

    Progress: Finish Section Hero MainPage

--- resources/views/mainpage/index.blade.php
+++ resources/views/mainpage/index.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="hero">
-        <div class="row">
-            <div class="col-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias provident eaque accusamus necessitatibus nesciunt veritatis laborum explicabo labore possimus harum! Alias sit neque debitis!</div>
-            <div class="col-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum iure aut repudiandae saepe voluptate minima nesciunt, magnam dolorem facere recusandae.</div>
+    <section class="hero d-flex align-items-center">
+        <div class="row align-items-center">
+            <div class="col">
+                <p class="subtitle">Celebrating Indonesia's Rich Artistic Heritage</p>
+                <h1 class="headline">Discovering Indonesia Through Art: Welcome to ArtistryIndo</h1>
+                <p class="paragraph">Explore the diverse and vibrant world of Indonesian art at ArtistryIndo. From traditional dance to modern installations, we showcase the soulful creativity that defines our nation. Join us on a visual and cultural journey that spans generations.</p>
+                <div class="button-group d-flex gap-2">
+                    <a href="#" class="button-primary">Explore Artistic Now</a>
+                    <a href="#" class="button-reverse">About ArtistryIndo</a>
+                </div>
+            </div>
+            <div class="col-lg-5 col-xxl-6 d-none d-lg-flex justify-content-end">
+                <img src="{{ asset('assets/images/banners/banner-hero.svg') }}" alt="Banner Hero" class="img-fluid banner-image">
+            </div>
         </div>
     </section>
 @endsection
