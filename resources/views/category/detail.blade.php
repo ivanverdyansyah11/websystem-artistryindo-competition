2ddd0cf2b99973b60d396f195bc42b9bab90a4f6
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-12-22 16:52:49 +0800

    Progress: Finish Section CTA Detail Category Page

--- resources/views/category/detail.blade.php
+++ resources/views/category/detail.blade.php
@@ -30,7 +30,124 @@
         </div>
     </section>
 
-    {{-- <section class="cta gap-section position-relative" id="cta">
+    <section class="category" id="category">
+        <div class="row gap-row">
+            <div class="col-md-6 col-lg-3">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-1.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Kecak Dance, Bali</h6>
+                    <p class="card-caption">Mesmerizing cultural spectacle known for its rhythmic chanting and intricate choreography.</p>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-4 mt-md-0">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-2.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Barong Dance, Gianyar, Bali</h6>
+                    <p class="card-caption">Performed in Gianyar, Bali, is a captivating portrayal of the eternal battle.</p>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-4 mt-lg-0">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-3.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Ramayana Performance, Prambanan</h6>
+                    <p class="card-caption">Mesmerizing cultural spectacle set against the backdrop of the majestic Prambanan Temple ...</p>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-4 mt-lg-0">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-4.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Martial arts</h6>
+                    <p class="card-caption">Martial arts, an ancient practice with roots in various cultures, is a disciplined and dynamic ...</p>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-4">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-5.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Reog Ponorogo, East Java</h6>
+                    <p class="card-caption">Reog Ponorogo, originating from East Java, is a vibrant and captivating traditional Indonesian ...</p>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-4">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-6.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Saman Dance, Aceh</h6>
+                    <p class="card-caption">The Saman Dance from Aceh, Indonesia, is a remarkable cultural expression characterized ...</p>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-4">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-7.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Plate Dance, West Sumatra</h6>
+                    <p class="card-caption">Tari Piring, originating from Sumatra Barat (West Sumatra), is a traditional Indonesian da ...</p>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-4">
+                <a href="#" class="card-category">
+                    <img src="{{ asset('assets/images/thumbnail-performings/thumbnail-performing-8.svg') }}" alt="Thumbnail Image" class="img-fluid w-100">
+                    <h6 class="card-title">Wayang Orang, Java</h6>
+                    <p class="card-caption">Explore the classical Javanese dance drama of Wayang Orang, where performers bring to lif ...</p>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section class="category gap-section pt-4" id="category">
+        <div class="row align-items-end justify-content-between">
+            <div class="col-lg-6">
+                <h3 class="title">Other Arts from Various Regions on ArtistryIndo</h3>
+            </div>
+            <div class="col-lg-5 col-xl-4 mt-3 mt-lg-0">
+                <p class="paragraph">Welcome to the Regional Arts section of ArtistryIndo, where every corner of the archipelago radiates its unique artistry. Let's collectively savor.</p>
+            </div>
+        </div>
+        <div class="row gap-row">
+            <div class="col-12 position-relative">
+                <div class="swiper mySwiper swiperCategory position-relative">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide p-1">
+                            <a href="#" class="category-item">
+                                <img src="{{ asset('assets/images/thumbnail-categories/thumbnail-category-1.svg') }}" alt="Thumbnail Category" class="img-fluid w-100">
+                                <h6>Performing Arts Heritage</h6>
+                            </a>
+                        </div>
+                        <div class="swiper-slide p-1">
+                            <a href="#" class="category-item">
+                                <img src="{{ asset('assets/images/thumbnail-categories/thumbnail-category-2.svg') }}" alt="Thumbnail Category" class="img-fluid w-100">
+                                <h6>Musical Arts Heritage</h6>
+                            </a>
+                        </div>
+                        <div class="swiper-slide p-1">
+                            <a href="#" class="category-item">
+                                <img src="{{ asset('assets/images/thumbnail-categories/thumbnail-category-3.svg') }}" alt="Thumbnail Category" class="img-fluid w-100">
+                                <h6>Traditional Fine Arts</h6>
+                            </a>
+                        </div>
+                        <div class="swiper-slide p-1">
+                            <a href="#" class="category-item">
+                                <img src="{{ asset('assets/images/thumbnail-categories/thumbnail-category-4.svg') }}" alt="Thumbnail Category" class="img-fluid w-100">
+                                <h6>Indonesian Literature and Folklore</h6>
+                            </a>
+                        </div>
+                        <div class="swiper-slide p-1">
+                            <a href="#" class="category-item">
+                                <img src="{{ asset('assets/images/thumbnail-categories/thumbnail-category-5.svg') }}" alt="Thumbnail Category" class="img-fluid w-100">
+                                <h6>Traditional Culinary Delights</h6>
+                            </a>
+                        </div>
+                        <div class="swiper-slide p-1">
+                            <a href="#" class="category-item">
+                                <img src="{{ asset('assets/images/thumbnail-categories/thumbnail-category-6.svg') }}" alt="Thumbnail Category" class="img-fluid w-100">
+                                <h6>Aksara and Literature</h6>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination swiper-pagination-category"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="cta gap-section position-relative" id="cta">
         <div class="style-1"></div>
         <div class="style-2"></div>
         <div class="row justify-content-center position-relative">
@@ -43,5 +160,34 @@
                 </div>
             </div>
         </div>
-    </section> --}}
+    </section>
+
+    @push('js')
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+        <script>
+            var swiper = new Swiper(".swiperCategory", {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                loop: true,
+                autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: false,
+                },
+                breakpoints: {
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 10,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 10,
+                    },
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            });
+        </script>
+    @endpush
 @endsection
